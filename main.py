9f8d5771dfef008f5b6ab81462d3bb2020639c00
title <wachirapanpimpa2530@gmail.com>
2023-12-22 09:21:04 +0700

    change_path_logger

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
             response = urlopen(path_url)
             data_json = json.loads(response.read())
 
-            plc_out_read = open('/home/pi/hottub_ma/txt_file/status_plc.txt','r')
+            plc_out_read = open('/home/linaro/hottub_linaro/txt_file/status_plc.txt','r')
             split_file_plc_out = plc_out_read.read().split(",")
             plc_q0 = split_file_plc_out[0].replace("[","")
             plc_q1 = split_file_plc_out[1]
@@ -27,7 +27,7 @@ while True:
             plc_q3 = split_file_plc_out[3].replace("]","")
             
             # plc in
-            plc_in_read = open('/home/pi/hottub_ma/txt_file/status_plc_in.txt','r')
+            plc_in_read = open('/home/linaro/hottub_linaro/txt_file/status_plc_in.txt','r')
             split_file_plc_in = plc_in_read.read().split(",")
             plc_i0 = split_file_plc_in[0].replace("[","")
             plc_i1 = split_file_plc_in[1]
@@ -38,7 +38,7 @@ while True:
             plc_i6 = split_file_plc_in[6]
             plc_i7 = split_file_plc_in[7].replace("]","")
             # relay
-            relay_read = open('/home/pi/hottub_ma/txt_file/status_relay_8.txt','r')
+            relay_read = open('/home/linaro/hottub_linaro/txt_file/status_relay_8.txt','r')
             split_file_relay = relay_read.read().split(",")
             relay_ch0 = split_file_relay[0].replace("[","")
             relay_ch1 = split_file_relay[1]
@@ -49,16 +49,16 @@ while True:
             relay_ch6 = split_file_relay[6]
             relay_ch7 = split_file_relay[7].replace("]","")
             # ph
-            read_ph = open('/home/pi/hottub_ma/txt_file/ph.txt','r')
+            read_ph = open('/home/linaro/hottub_linaro/txt_file/ph.txt','r')
             ph_data = read_ph.read()
             # orp
-            read_orp = open('/home/pi/hottub_ma/txt_file/orp.txt','r')
+            read_orp = open('/home/linaro/hottub_linaro/txt_file/orp.txt','r')
             orp_data = read_orp.read()
             # temp
-            read_temp = open('/home/pi/hottub_ma/txt_file/temperature.txt','r')
+            read_temp = open('/home/linaro/hottub_linaro/txt_file/temperature.txt','r')
             temp_data = read_temp.read()
             #pressure
-            read_pressure = open('/home/pi/hottub_ma/txt_file/pressure.txt','r')
+            read_pressure = open('/home/linaro/hottub_linaro/txt_file/pressure.txt','r')
             pressure_data = read_pressure.read()
 
             if int(data_json[0]['online_status']) == 1:
